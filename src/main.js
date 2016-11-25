e8a213e0d3207a72ea2607c9a1f7c9c3a16b549c
Sawyer666 <alex84824@gmail.com>
2016-11-25 12:54:31 +0300

    asdasd

---
+++ src/main.js
@@ -0,0 +1,14 @@
+
+
+import angular from 'angular';
+
+console.log(angular);
+const ngModule = angular.module('app', []);
+//     .directive('search', searchDirective);
+//
+// function searchDirective() {
+//     return {
+//         restrict: 'E',
+//         template: '<div>111</div>'
+//     }
+// }
